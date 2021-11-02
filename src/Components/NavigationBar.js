d15584b801ffbe3e046b72c67bc5bd375636eb10
FomasTreeman <tom@team-freeman.com>
2021-11-02 16:16:44 +0000

    separated navbar and routing to individual components

---
+++ src/Components/NavigationBar.js
@@ -0,0 +1,108 @@
+import { useState } from "react"
+import { Search } from 'react-bootstrap-icons';
+import styled, { keyframes } from 'styled-components';
+import { tada } from 'react-animations';
+import {
+    Container,
+    Nav,
+    Navbar,
+    NavDropdown,
+    Form,
+    Button
+} from 'react-bootstrap'
+import {
+    useHistory,
+    Link
+} from 'react-router-dom';
+
+/** ////////////////////////////////////
+* ?  Animation}
+ */////////////////////////////////////
+
+const tadaAnimation = keyframes`${tada}`;
+
+// function call using `` instead of $()
+let BrandName = styled.p`
+   animation: 1s ${tadaAnimation};
+   animation-iteration-count: infinite;
+   margin: 0;
+   `;
+
+
+function NavigationBar(props) {
+    const history = useHistory();
+    const [searchValue, setSearchValue] = useState("");
+    
+    let paths = props.paths;
+    let projects = props.projects;
+
+    function DropdownList() {
+        var items = [];
+        for (let i = 4; i > 0; i--) {
+            items.push(<Nav.Link key={i} as={Link} className="p-1 pl-0 m-auto" to={paths[paths.length - i].path}>{paths[paths.length - i].label}</Nav.Link>)
+        }
+        return <div>{items}</div>
+    }
+
+    const searchSubmit = (event) => {
+        let matchingElement = null;
+        if (searchValue && searchValue.length > 1) {
+            matchingElement = paths.find(element => {
+                return element.label.toLowerCase().includes(searchValue.toLowerCase());
+            })
+        }
+        matchingElement ? history.push(matchingElement.path) : event.preventDefault();
+    }
+
+
+    return (
+        <Navbar id="navigation" className="justify-content-center" bg="info" expand="sm" sticky="top" >
+            <Container fluid className="pr-0">
+                <Navbar.Brand as={Link} to={paths[3].path} bg="success" id="navigation-brand">
+                    <BrandName> Tom's Portfolio </BrandName>
+                </Navbar.Brand>
+                <Navbar.Toggle className="px-2" />
+                <Navbar.Collapse className="justify-content-end">
+                    <Nav className="pr-3" style={{ maxHeight: '100px' }}>
+                        <NavDropdown title="My projects " id="small-print" align="end">
+                            {
+                                projects.map((element, index) => {
+                                    return (
+                                        <Nav.Link
+                                            key={index}
+                                            as={Link}
+                                            className="px-3"
+                                            id="dropdownItem"
+                                            to={element.path}
+                                        >
+                                            {element.label}
+                                        </Nav.Link>
+                                    )
+                                })
+                            }
+                            <div key="-1" className="m-0 p-0 pl-3">
+                                <NavDropdown.Divider />
+                                <DropdownList />
+                            </div>
+                        </NavDropdown>
+                    </Nav>
+                    <Form className="d-flex" id="small-print" onSubmit={searchSubmit} >
+                        <Form.Control
+                            type="search"
+                            placeholder="Search"
+                            className="mr-2"
+                            aria-label="Search"
+                            value={searchValue}
+                            onChange={({ target: { value } }) => { setSearchValue(value); }}
+                        />
+                        <Button type="submit" variant="outline-secondary">
+                            <Search />
+                        </Button>
+                    </Form>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    )
+}
+
+export default NavigationBar
